47062985b7390c4d1588888738c5e732a403c9d9
李真真 <731466760@qq.com>
2017-08-03 20:53:28 +0800

    170803L

--- src/moreGoods_page/classify_filtrate_model.jsx
+++ src/moreGoods_page/classify_filtrate_model.jsx
@@ -1,6 +1,28 @@
 import React, {Component} from 'react';
+import {Icon} from 'antd';
 import './classify_filtrate_model.css'
+
+const SHOWTEXT = "显示筛选";
+const CLOSETEXT = "收起筛选";
+
 class ClassifyFiltrateModel extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            showFiltrateButton: SHOWTEXT,
+        }
+    }
+
+    handleShowClick=()=>{
+        var nowText = this.state.showFiltrateButton;
+        if(nowText===SHOWTEXT){
+            this.setState({showFiltrateButton:CLOSETEXT})
+        }else{
+            this.setState({showFiltrateButton:SHOWTEXT})
+        }
+    }
+
+
     render() {
         return (
             <div className=''>
@@ -13,7 +35,18 @@ class ClassifyFiltrateModel extends Component {
                 </div>
                 <div className='all-classify-menu'>
                     <div className='filtrate-condition-row'>
-                        <div>所有分类></div>
+                        <a className="expand-collapse" onClick={this.handleShowClick}>
+                            <span>
+                                <span>{this.state.showFiltrateButton}</span>
+                                <Icon type="down" className="expand-collapse-down" />
+                            </span>
+                            
+                            {/* <span>
+                                <span>收起筛选</span>
+                                <span></span>
+                            </span> */}
+                        </a>
+                        <span>所有分类></span>
                     </div>
                     <div className='filtrate-groups-box'>
                         <div className='group'>
